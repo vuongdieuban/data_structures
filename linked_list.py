7928895dfbc149a8279582942620b9d9dc9287a7
Ban Vuong <vuongdieuban@gmail.com>
2018-10-18 21:15:21 -0600

    linked_list: add insertAtBeginning()

--- linked_list.py
+++ linked_list.py
@@ -1,3 +1,8 @@
+"""
+WRITING LINKED LIST FROM SCRATCH
+"""
+
+
 class Node:
     def __init__(self, initdata):
         self.data = initdata
@@ -8,6 +13,7 @@ class UnorderedList(Node):
     """"
     functions:
         def insertAtEnd(): add item to the end of the list, similar to append
+        def insertAtBeginning(): add item to the beginning of the list
         def pop(): remove at the end node (last in first out)
         def print() : traverse the list and print data at every node
     """
@@ -29,16 +35,30 @@ class UnorderedList(Node):
             return
 
         if self.head is not None:
-            while last_node.next is not None:
+            while last_node.next is not None:   # use last_node to traverse the list and link to new node
                 last_node = last_node.next
             last_node.next = new_node
+            return
+
+    def insertAtBeginning(self, item):
+        new_node = Node(item)
+        new_node.data = item
+
+        if self.head is None:
+            new_node.next = None
+            self.head = new_node
+            return
+
+        if self.head is not None:
+            new_node.next = self.head
+            self.head = new_node
             return
 
     def print(self):
         temp = self.head
         print("List is:")
         while temp is not None:
-            print(temp.data, "")
+            print(temp.data)
             temp = temp.next
         return
 
@@ -49,4 +69,6 @@ if __name__ == '__main__':
     mylist.insertAtEnd(7)
     mylist.insertAtEnd(10)
     mylist.insertAtEnd(11)
+    mylist.insertAtBeginning(8)
+    mylist.insertAtBeginning(2)
     mylist.print()
